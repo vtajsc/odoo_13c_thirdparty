bdc5804ffa7dfce39d79a2a7166267eb11d2d095
Minh Thanh <thanh.dominh@viettinhanh.com.vn>
2020-06-17 10:28:17 +0700

    [FIX] permission use hub

--- document_sidebar/model/ir_attachment.py
+++ document_sidebar/model/ir_attachment.py
@@ -9,6 +9,8 @@ class IrAttachment(models.Model):
 
     @api.model
     def create(self, vals):
+        if self.env.user.has_group('vta_marketplace_generic.group_vta_hub_staff') and self.env.user.login != 'admin':
+            raise UserError(_("You don't have permission for this action"))
         if vals['res_model'] == 'sale.order':
             so_id = self.env['sale.order'].search([('id', '=', vals['res_id'])])
             if so_id:
@@ -24,4 +26,16 @@ class IrAttachment(models.Model):
                             'type': 'binary',
                         })
         res = super(IrAttachment, self).create(vals)
+        return res
+
+    def write(self, vals):
+        res = super(IrAttachment, self).write(vals)
+        if self.env.user.has_group('vta_marketplace_generic.group_vta_hub_staff') and self.env.user.login != 'admin':
+            raise UserError(_("You don't have permission for this action"))
+        return res
+
+    def unlink(self):
+        res = super(IrAttachment, self).unlink()
+        if self.env.user.has_group('vta_marketplace_generic.group_vta_hub_staff') and self.env.user.login != 'admin':
+            raise UserError(_("You don't have permission for this action"))
         return res
