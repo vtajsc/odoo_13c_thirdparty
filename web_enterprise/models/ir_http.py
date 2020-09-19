00e80d27d7916aeb52d629d27ba0e792e8e39a12
Hoang Vu Vo <vu.vohoang@viettinhanh.com.vn>
2020-09-19 12:10:11 +0700

    [FIX] web_enterprise
    * show action sidebar
    * fix represent wrong version

--- web_enterprise/models/ir_http.py
+++ web_enterprise/models/ir_http.py
@@ -18,7 +18,7 @@ class IrHttp(models.AbstractModel):
     def session_info(self):
         info = super(IrHttp, self).session_info()
         info.update({
-            'server_version': '%s+e' % info['server_version'],
+            'server_version': '%s+e' % info['server_version'].replace('+e', ''),
             'server_version_info': list(info['server_version_info'][:-2]) + ['e'],
         })
         return info
