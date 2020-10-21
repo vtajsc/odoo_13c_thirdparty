3aff3afb780259af139a6899d841761bae7db9eb
Minh Thanh <thanhdmvta>
2020-10-21 09:12:59 +0700

    [FIX] ir attachment check model

--- document_sidebar/model/ir_attachment.py
+++ document_sidebar/model/ir_attachment.py
@@ -9,9 +9,9 @@ class IrAttachment(models.Model):
 
     @api.model
     def create(self, vals):
-        if self.env.user.has_group('vta_marketplace_generic.group_vta_hub_staff') and self.env.user.login != 'admin':
-            raise UserError(_("You don't have permission for this action"))
-        if vals['res_model'] == 'sale.order':
+        if vals['res_model'] in ('sale.order', 'stock.picking'):
+            if self.env.user.has_group('vta_marketplace_generic.group_vta_hub_staff') and self.env.user.login != 'admin':
+                raise UserError(_("You don't have permission for this action"))
             so_id = self.env['sale.order'].search([('id', '=', vals['res_id'])])
             #Trong - 1108 - Cho phep add file vận đơn đối vs các đơn thiếu tồn, hàng thiếu
             if so_id and so_id.stage_id and so_id.stage_id.code in ('open', 'inventory_not_enough', 'low_quality_product'):
@@ -28,19 +28,31 @@ class IrAttachment(models.Model):
                         })
                 res = super(IrAttachment, self).create(vals)
             else:
-                res = super(IrAttachment, self).create([])
+                if so_id and so_id.mapped('picking_ids') and (self.env.user.login == 'admin' or self.env.user.id == 1):
+                    pickings = so_id.mapped('picking_ids')
+                    for pic in pickings:
+                        self.create({
+                            'name': vals['name'],
+                            'res_id': pic.id,
+                            'res_model': pic._name,
+                            'datas': vals['datas'],
+                            'type': 'binary',
+                        })
+                    res = super(IrAttachment, self).create(vals)
+                else:
+                    res = super(IrAttachment, self).create([])
         else:
             res = super(IrAttachment, self).create(vals)
         return res
 
     def write(self, vals):
-        res = super(IrAttachment, self).write(vals)
-        if self.env.user.has_group('vta_marketplace_generic.group_vta_hub_staff') and self.env.user.login != 'admin':
-            raise UserError(_("You don't have permission for this action"))
-        return res
+        if self.res_model in ('sale.order', 'stock.picking'):
+            if self.env.user.has_group('vta_marketplace_generic.group_vta_hub_staff') and self.env.user.login != 'admin':
+                raise UserError(_("You don't have permission for this action"))
+        return super(IrAttachment, self).write(vals)
 
     def unlink(self):
-        res = super(IrAttachment, self).unlink()
-        if self.env.user.has_group('vta_marketplace_generic.group_vta_hub_staff') and self.env.user.login != 'admin':
-            raise UserError(_("You don't have permission for this action"))
-        return res
+        if self.res_model in ('sale.order', 'stock.picking'):
+            if self.env.user.has_group('vta_marketplace_generic.group_vta_hub_staff') and self.env.user.login != 'admin':
+                raise UserError(_("You don't have permission for this action"))
+        return super(IrAttachment, self).unlink()
